fcf5b24aef887476ae3abe12633a218ca93acebb
lukas-sr <lukas-sr@hotmail.com>
2020-03-17 17:22:01 -0300

    Atualização main.c

--- src/main.c
+++ src/main.c
@@ -1,14 +1,49 @@
-/* Data de submissao:
- * Nome:
- * RA:
+/* Nome: Lukas Rosa
+ * RA: 183167
  */
 
 #include <stdio.h>
-const int tam_buffer=100;
+#include <string.h>
+#define tam_buffer 100
+
+char pilha[tam_buffer];
+int n_pilha=0;
+
+void push(char c) {
+ //Insere caractere c na pilha
+  if (n_pilha < tam_buffer) {
+    pilha[n_pilha]=c;
+    n_pilha++;
+  }
+}
+
+char pop() {
+ //Remove caractere do topo da pilha
+  char c;
+  if (n_pilha>0) {
+    n_pilha--;
+    c = pilha[n_pilha];
+
+    return c;
+  }
+
+}
 
 int main() {
+  unsigned int i, j;
   char buffer[tam_buffer];
+
   fgets(buffer, tam_buffer, stdin);
-  printf("%s", buffer);
+
+  for ( i = 0 ; i < (strlen(buffer)) ; i++){
+    if (!(buffer[i] == ' ' || buffer[i] == '\n' || buffer[i] == '\0')) push(buffer[i]);
+
+    else{
+        for ( j = 0 ; j< (strlen(buffer)) ; j++) printf("%c", pop());
+
+        if (i != strlen(buffer)-1) printf(" ");
+    }
+  }
+  printf("\n");
   return 0;
 }
